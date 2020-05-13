0609f3a6aee414a638ee972151e64e67fbfdea18
jeromeDevIN <jerome.tonin@gmail.com>
2020-05-13 10:16:17 +0200

    Update Jansson_TEST.cpp

--- Jansson_TEST.cpp
+++ Jansson_TEST.cpp
@@ -3,3 +3,14 @@
 
 #define URL_FORMAT   "https://api.github.com/repos/%s/%s/commits"
 #define URL_SIZE     256
+
+/* Return the offset of the first newline in text or the length of
+   text if there's no newline */
+static int newline_offset(const char *text)
+{
+    const char *newline = strchr(text, '\n');
+    if(!newline)
+        return strlen(text);
+    else
+        return (int)(newline - text);
+}
